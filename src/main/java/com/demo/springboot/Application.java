b2465ce9493738c2c563c9abe64858de8088c552
yanming-work <yanming_work@163.com>
2017-07-27 14:55:18 +0800

    增加修改端口
    增加修改端口

--- src/main/java/com/demo/springboot/Application.java
+++ src/main/java/com/demo/springboot/Application.java
@@ -2,6 +2,10 @@ package com.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Spring Boot 应用启动类
@@ -10,7 +14,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Spring Boot 应用的标识
 @SpringBootApplication
 
-public class Application {
+public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {  
+  
+    @Override  
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {  
+        return builder.sources(Application.class);  
+    }  
+      
+  
+  
+    @Override  
+    public void customize(ConfigurableEmbeddedServletContainer container) {  
+        container.setPort(8080);  //设置服务的端口，默认为8080
+    }  
+
+
 
     public static void main(String[] args) {
         // 程序启动入口
